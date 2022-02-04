8de42e6a8cb13ee413b2d44c90d1447f8dface7b
Mahdi <shirani98@yahoo.com>
2022-02-04 13:35:25 +0330

    fix avatar bug

--- social/settings.py
+++ social/settings.py
@@ -168,3 +168,14 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'urusername@gmail.com'
 EMAIL_HOST_PASSWORD = 'password'
+
+#https csrf config
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1']
+
+#Arvan cloud storage (like AWS servise)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'instatest'
+AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
+AWS_S3_FILE_OVERWRITE = False
